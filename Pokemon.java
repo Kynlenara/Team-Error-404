3c2bb1d7bb28259556a17df56267fda7fd7a185d
Kynlenara <christinasimino@gmail.com>
2024-01-20 13:41:16 -0600

    Some basic structure

---
+++ Pokemon.java
@@ -0,0 +1,14 @@
+public class Pokemon {
+    String name;
+    String type;
+    String strong;
+    String weak;
+
+    public Pokemon(String name, String type, String strong, String weak) {
+        this.name = name;
+        this.type = type;
+        this.strong = strong;
+        this.weak = weak;
+    }
+
+}
--- main.java
+++ main.java
@@ -1,8 +1,14 @@
 
-
 class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+
+        Pokemon pika = new Pokemon("Pikachu", "Lightning", "Ground", "Flying");
+
+        System.out.println(pika.name);
+        System.out.println(pika.type);
+        System.out.println(pika.strong);
+        System.out.println(pika.weak);
     }
 }
